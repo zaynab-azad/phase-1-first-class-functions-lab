fdefb68d5ad103ba2d7c87cf1c8e2d2b003fd45a
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-11-23 21:55:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function returnFirstTwoDrivers(drivers) {
+  return [...drivers.slice(0, 2)];
+};
+const returnLastTwoDrivers = function returnLastTwoDrivers(drivers) {
+  return [...drivers.slice(drivers.length - 2, drivers.length)];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+  return function () {
+    return num * num;
+  };
+}
+
+function fareDoubler(num) {
+  return num * 2;
+}
+function fareTripler(num) {
+  return num * 3;
+}
+function selectDifferentDrivers(drivers, funcs) {
+  if (funcs == returnFirstTwoDrivers) {
+    return drivers.slice(0, 2);
+  } else if (funcs == returnLastTwoDrivers) {
+    return drivers.slice(drivers.length - 2, drivers.length);
+  }
+}
